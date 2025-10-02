c88ece4b4a2628df234d47067a5003bda3664ef8
MakarPoleyloKovalev <makarkovalev28@gmail.com>
2025-10-02 13:19:52 +0300

    Update hello_world.py

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,3 +1,9 @@
-def text():
-    VAR = message
-    return VAR
+import unittest
+
+import hello_world
+
+
+class HelloTestCase(unittest.TestCase):
+    def test_hello(self):
+        m = "message"
+        self.assertEqual(m, hello_world.text())
